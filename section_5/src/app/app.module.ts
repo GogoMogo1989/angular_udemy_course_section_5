34af84f68ec5ee875eae84bae98c8d8300092dc5
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-09-26 15:59:39 +0200

    20230926

---
+++ section_5/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent, CockpitComponent, ServerElementComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
